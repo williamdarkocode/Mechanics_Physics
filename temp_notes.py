eda777dfd24fb0552da711d09f501d1da36339cd
William <williamd8323@afsenyc.org>
2021-08-26 13:21:50 -0400

    lecture 1

---
+++ temp_notes.py
@@ -0,0 +1,70 @@
+#### Chapter 2: Kinematics in 1 dimension (Rectilinear Motion)
+"""
+space+time create an event/occurrence
+
+particles (objects), mass
+
+units bear great importance in physics
+
+units of space, distance such as metre:
+
+MKS: metre, kilogram, second
+
+CGS: cm, gram,
+
+All physical quantities in this course are describable using units: Metres, Kilogramss, seconds
+
+learning outcomes:
+-describe what a particle is
+-define position, distance, displacement, speed, velocity, and acceleration of a particle in rectilinear motion
+-write relationships between postion and velocity of particle in rectilinear motion under uniform constant acceleration as function of time
+-solve for two of them given third. requires an understanding of appropriate initial conditions in each case
+-describe physical interpretation of position, velocity, and acceleration of particle
+-identify and analyse special case of rectilinear motion
+-solve problems involving uniform acceleartion, acceleration due to gravity with no resistance
+
+
+**Reference frame**
+reference frame is coordinate system relative to  which motion and rest may be measured; measurement of position, distance, speed, must be made with respect to reference frame.
+
+Concept of Inertial reference frame
+--- moving at rate/speed without changing direction
+
+
+
+**distinction between distance, displacement**:
+-displacement is how far object is from starting point, regardless of how it got there
+-distnace traveled is the length of the actual path, or measured along the actual path traveled
+
+
+-displacement is written as: \delta[x] = x_2 - x_1
+-displacement depends on initial, and final position only
+
+-displacement is a vector; vector is quantity with magnitude and direction; scalar is quantity of only magnitude
+
+
+one dimensional motion = rectilinear motion = linear motion
+where movement happens only on one spatial dimesion
+
+avg speed = distance travelled/time elapsed = d/(delta t) where d is distance, and t is time
+
+avg velocity = displacement/time elapsed = delta x / delta t
+
+57 
+
+avg spd = 57/10.8 = 5.27 mps
+svg velo = 19/10.8 = 1.75 msps
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
